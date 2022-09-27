4dfe88e8188740768f5046972dc72739f84f895b
amnivore <amnahaider52@gmail.com>
2022-09-26 20:58:50 -0400

    added a syso

---
+++ week1/HelloWorld.java
@@ -0,0 +1,13 @@
+package week1;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("It is Thursday");
+    }
+    
+    
+}
+
